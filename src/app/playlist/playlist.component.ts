0484971b4c140612f117a9d1d008605c016419b1
Unknown <chretien.michel2@gmail.com>
2018-11-04 21:35:04 +0100

    Création page playlists, nouvelle playlist, ajout et affichage

--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {User} from 'firebase';
 import {FirebaseService} from '../firebase.service';
+import {FirebaseListObservable} from '@angular/fire/database-deprecated';
+import {AngularFireDatabase} from '@angular/fire/database';
+import {MoviesList} from './MoviesList';
+import {MovieResponse} from '../tmdb-data/Movie';
 
 @Component({
   selector: 'app-playlist',
@@ -10,12 +14,37 @@ import {FirebaseService} from '../firebase.service';
 export class PlaylistComponent implements OnInit {
 
   @Input() fs: FirebaseService;
+  private rawPlaylists: JSON;
+  public playlists: MoviesList[] = [];
   constructor() {
-
   }
 
   ngOnInit() {
-    console.log(this.fs);
+    console.log('Playlist ' + this.fs);
+    this.fs.getAllPlaylist().then(val => {
+      this.rawPlaylists = val.val();
+      const lists = Object.keys( this.rawPlaylists);
+      for (const l of lists) {
+        const playlist: MoviesList = {
+          name : l,
+          description : this.rawPlaylists[l]['description'],
+          movies: []
+        };
+        console.log(playlist);
+          console.log(this.rawPlaylists[l].films);
+        for (const f in this.rawPlaylists[l].films) {
+          const m: MovieResponse = <MovieResponse> this.rawPlaylists[l].films[f];
+          playlist.movies.push(m);
+        }
+        /*for (const m of moviesLists) {
+          playlist.movies.push(<MovieResponse> m);
+        }*/
+        this.playlists.push(playlist);
+      }
+    });
   }
 
+  public createPlaylist(listName: string, listDesc: string) {
+    this.fs.createPlaylist(listName, listDesc);
+  }
 }
