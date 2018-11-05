352e2f6056e061efbc02cc5f4cbc8c81b537883d
Unknown <chretien.michel2@gmail.com>
2018-11-05 22:23:22 +0100

    Page playlist utilise playlist et plus film-list pour afficher les films

--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -11,37 +11,12 @@ import {MovieResponse} from '../tmdb-data/Movie';
 export class PlaylistComponent implements OnInit {
 
   @Input() fs: FirebaseService;
-  private rawPlaylists: JSON;
-  public playlists: MoviesList[] = [];
+  @Input() playlist: MoviesList;
   constructor() {
   }
 
   ngOnInit() {
-    console.log('Playlist ' + this.fs);
-    this.fs.getAllPlaylist().then(val => {
-      this.rawPlaylists = val.val();
-      const lists = Object.keys( this.rawPlaylists);
-      for (const l of lists) {
-        const playlist: MoviesList = {
-          name : l,
-          description : this.rawPlaylists[l]['description'],
-          movies: []
-        };
-        console.log(playlist);
-          console.log(this.rawPlaylists[l].films);
-        for (const f in this.rawPlaylists[l].films) {
-          const m: MovieResponse = <MovieResponse> this.rawPlaylists[l].films[f];
-          playlist.movies.push(m);
-        }
-        /*for (const m of moviesLists) {
-          playlist.movies.push(<MovieResponse> m);
-        }*/
-        this.playlists.push(playlist);
-      }
-    });
+    console.log(this.playlist);
   }
 
-  public createPlaylist(listName: string, listDesc: string) {
-    this.fs.createPlaylist(listName, listDesc);
-  }
 }
