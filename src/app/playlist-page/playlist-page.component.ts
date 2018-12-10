aef68c7d453658b0628edfe3c64779aab0651756
Unknown <chretien.michel2@gmail.com>
2018-12-10 22:17:03 +0100

    Factorisation & update de l'ajout de playlist pour ne pas avoir à reload

--- src/app/playlist-page/playlist-page.component.ts
+++ src/app/playlist-page/playlist-page.component.ts
@@ -4,6 +4,8 @@ import {MovieResponse} from '../tmdb-data/Movie';
 import {MoviesList} from '../playlist/MoviesList';
 import {MatSnackBar} from '@angular/material';
 import {Firebase2Service} from '../firebase2.service';
+import DataSnapshot = firebase.database.DataSnapshot;
+import {Log} from '@angular/core/testing/src/logger';
 
 @Component({
   selector: 'app-playlist-page',
@@ -29,31 +31,18 @@ export class PlaylistPageComponent implements OnInit {
 
   ngOnInit() {
     console.log('Playlist ' + this.fs);
-    this.fb.getPlaylist().then(val => {
-      this.rawPlaylists = val.val();
-      const lists = Object.keys( this.rawPlaylists);
-      for (const l of lists) {
-        const playlist: MoviesList = {
-          name : l,
-          description : this.rawPlaylists[l]['description'],
-          movies: []
-        };
-        console.log(playlist);
-        console.log(this.rawPlaylists[l].films);
-        for (const f in this.rawPlaylists[l].films) {
-          const m: MovieResponse = <MovieResponse> this.rawPlaylists[l].films[f];
-          playlist.movies.push(m);
-        }
-        /*for (const m of moviesLists) {
-          playlist.movies.push(<MovieResponse> m);
-        }*/
-        this.playlists.push(playlist);
-      }
-      this.slicedPlaylists = this.playlists.slice(0, this.numberOfFilmTOShow);
-      this.cursor = 0;
-    });
+    this.updatePlaylist();
   }
 
+  updatePlaylist() {
+    this.fb.getObjectPlaylist().then(val => {
+      this.playlists = val;
+      if (this.playlists != null) {
+        this.slicedPlaylists = this.playlists.slice(0, this.numberOfFilmTOShow);
+        this.cursor = 0;
+      }
+    });
+  }
   openSnackBar(message: string, action: string) {
     this.snackBar.open(message, action, {
       duration: 2000,
@@ -62,9 +51,10 @@ export class PlaylistPageComponent implements OnInit {
 
   public createPlaylist() {
     console.log("Create Playlist");
-    const play: MoviesList = { name : this.title};
-    this.playlists.push(play);
-    this.fb.createPlaylist( this.title,  this.desc).then(() => this.openSnackBar('Playlist ajouté !',''));
+    this.fb.createPlaylist( this.title,  this.desc).then(() => {
+        this.openSnackBar('Playlist ajouté !','');
+        this.updatePlaylist();
+     });
     this.desc = ' ';
     this.title = ' ';
   }
